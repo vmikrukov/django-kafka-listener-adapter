64530fac40059b85c6b26b1bb4d47afee5777021
Vladimir Mikryukov <motobiker2008@gmail.com>
2020-12-08 10:38:00 +0300

    Added lib description

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # django-kafka-listener-adapter
 This project helps you easily listen kafka topics in your django app
+
+Create your own class from **KafkaAdapter** and override two class methods:
+
+Example:
+```
+class KafkaListenerAdapter(KafkaAdapter):
+
+    @classmethod
+    def get_available_kafka_handlers(cls):
+        pass
+
+    @classmethod
+    def handle(cls, payload, handler):
+        pass
+```
+                                         
+Then you can run listening as a command in separate docker-container:
+
+```
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        KafkaListenerAdapter.start()
+```
+
--- setup.py
+++ setup.py
@@ -1,3 +1,23 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup()
+setup(
+    name='django-kafka-listener-adapter',
+    version='0.3.1',
+    description='A Django app to listen kafka topics.',
+    author='Vladimir Mikryukov',
+    author_email='motobiker2008@gmail.com',
+    license='Apache v.2',
+    classifiers=[
+        'Framework :: Django'
+        'Intended Audience :: Developers'
+        'License :: OSI Approved :: Apache License'
+        'Operating System :: OS Independent'
+        'Programming Language :: Python :: 3'
+        'Programming Language :: Python :: 3.6'
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    url='https://github.com/vmikrukov/django-kafka-listener-adapter',
+    install_requires=['kafka-python']
+
+)
